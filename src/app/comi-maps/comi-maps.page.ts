e0755c2e083cbcc4c5b0dbec45e56893de0970c9
Abraham-125 <83732740+Abraham-125@users.noreply.github.com>
2024-12-14 10:47:07 -0300

    Update comi-maps.page.ts

--- src/app/comi-maps/comi-maps.page.ts
+++ src/app/comi-maps/comi-maps.page.ts
@@ -16,10 +16,8 @@ export class ComiMapsPage implements OnInit {
   }
 
   logout() {
-    // Limpia los datos del usuario (localStorage o cualquier otra lógica que tengas)
     localStorage.clear();
 
-    // Redirecciona a la página de inicio de sesión
     this.router.navigate(['/']);
   }
 }
